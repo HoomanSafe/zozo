57fe330c5984ee6dff1200cbe59b4f8441069894
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-21 16:03:11 +0800

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="py-[20px] w-full font-primary">
       <div className="container text-center flex justify-center items-center">
-        <p>&copy; copyright 2023 black-ether. all right reserved</p>
+        <p>&copy; copyright 2024 ZoZo. all right reserved</p>
       </div>
     </footer>
   );
